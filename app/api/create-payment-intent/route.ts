b4440dcc2f1786373ebef3988eb2d43d8dd6f60f
Jorge Peraza <jorge@monkeys.cloud>
2026-01-21 17:37:07 -0700

    fix stripe version

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -4,7 +4,7 @@ import { promises as fs } from "fs"
 import path from "path"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-  apiVersion: "2024-12-18.acacia",
+  apiVersion: "2025-12-15.clover",
 })
 
 // Function to send email notification via MonkeysMail
